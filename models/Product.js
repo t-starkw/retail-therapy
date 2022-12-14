1a4f52702a2e1385073ad7318e0b97654bc339fa
tessa <tess.starkweather@gmail.com>
2022-12-14 18:31:48 -0500

    establish fields and rules for models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,44 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      // int, not null, primary, auto incr
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      // string, notnull
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      // decimal, not null, VALIDATES decimal
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      // int, not null, DEFAULT = 10, VALIDATES numeric value
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      // int, REFERENCES category model id
+      type: DataTypes.INTEGER,
+
+      references: {
+        model: 'Category',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
