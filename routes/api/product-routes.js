7b4ff794874fdae2051e3fb1a4b4669f3a2735bd
tessa <tess.starkweather@gmail.com>
2022-12-14 22:01:04 -0500

    defined routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,60 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+    {
+      model: Category,
+      attributes: ['id', 'category_name'],
+    },
+    {
+      model: Tag,
+      through: ProductTag,
+      as: 'tags'
+    },
+  ]
+  })
+    .then((dbProdData) => res.json(dbProdData))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name'],
+      },
+      {
+        model: Tag,
+        through: ProductTag,
+        as: 'tags'
+      },
+  ],
+  })
+  .then((dbProdData) => {
+    if(!dbProdData) {
+      res
+        .status(404)
+        .json({ message: "No product associated with this ID."});
+      return;
+    }
+    res.json(dbProdData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -91,6 +139,24 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((dbProdData) => {
+      if(!dbProdData) {
+        res
+          .status(404)
+          .json({ message: "No product associated with this ID."});
+        return;
+      }
+      res.json(dbProdData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
